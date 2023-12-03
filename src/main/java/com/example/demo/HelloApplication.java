bfbbbea0664c8f772becdd05d5da07039b629ef9
John Roberton <johnroberton@Johns-MacBook-Pro.local>
2023-12-02 21:57:09 -0700

    Initial commit

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
