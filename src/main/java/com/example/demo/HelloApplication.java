1dd056f0889c33f07ab455d4f092da14657338eb
John Roberton <johnroberton@Johns-MacBook-Pro.local>
2023-12-02 22:46:46 -0700

    added a loops

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -7,24 +7,42 @@ public class HelloApplication {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12; // These are constant variables, meaning they do not change
         final byte PERCENT = 100;
+        int principal = 0;
 
         Scanner scanner = new Scanner(System.in);
 
+        // Principal Input Loop
         while (true) {
             System.out.println("Principal: ");
-            int principal = scanner.nextInt();
+            principal = scanner.nextInt();
             if (principal >= 1000 && principal <= 1_000_000)
                 break;
             System.out.println("Enter a value between 1000 and 1000000");
+        }
 
-        System.out.println("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+        // Annual Interest Rate Input Loop
+        float annualInterest;
+        while (true) {
+            System.out.println("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30)
+                break;
+            System.out.println("Enter a value between 0 and 30");
+        }
+
+        // Period (Years) Input Loop
+        byte years;
+        while (true) {
+            System.out.println("Period (Years): ");
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30)
+                break;
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+        // Mortgage Calculation
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.println("Period (Years): ");
-        byte years = scanner.nextByte();
         int numberOfPayments = years * MONTHS_IN_YEAR;
-
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments));
